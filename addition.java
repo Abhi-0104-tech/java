76abb7931f46d6ef132b4e85812e72b84127d79b
Abhishek T S <137434717+Abhi-0104-tech@users.noreply.github.com>
2024-02-09 20:20:33 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class addition{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two munbers");
+    
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum= a+b;
+        System.out.println("sum of two num "+sum);
+}
+}
